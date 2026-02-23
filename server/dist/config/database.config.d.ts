a09b878084c49d26eb52fcfa2c82ea704a8b884d
VishwaChakrani14 <146700853+VishwaChakrani14@users.noreply.github.com>
2026-02-23 12:40:58 +0530

    Delete server/dist directory

--- server/dist/config/database.config.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const connectDatabase: () => Promise<void>;
-//# sourceMappingURL=database.config.d.ts.map
--- server/dist/utils/email.utility.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class EmailUtility {
-    private static transporter;
-    static sendVerificationOTP(email: string, otp: string, name: string): Promise<void>;
-    static sendResetOTP(email: string, otp: string, name: string): Promise<void>;
-}
-//# sourceMappingURL=email.utility.d.ts.map
